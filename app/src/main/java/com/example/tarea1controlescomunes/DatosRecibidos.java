6f765932809988bc23aec305bf8b0954156ee00c
Alexander <aherreras3@uteq.edu.ec>
2022-11-28 22:10:20 -0500

    Tarea1

---
+++ app/src/main/java/com/example/tarea1controlescomunes/DatosRecibidos.java
@@ -0,0 +1,36 @@
+package com.example.tarea1controlescomunes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DatosRecibidos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos_recibidos);
+        TextView lblsaludo = (TextView) findViewById(R.id.lblsaludo);
+        TextView lblsaludo2 = (TextView) findViewById(R.id.lblsaludo2);
+        TextView lblsaludo3 = (TextView) findViewById(R.id.lblsaludo3);
+        TextView lblsaludo4 = (TextView) findViewById(R.id.lblsaludo4);
+        TextView lblsaludo5 = (TextView) findViewById(R.id.lblsaludo5);
+        TextView lblsaludo6 = (TextView) findViewById(R.id.lblsaludo6);
+        TextView lblsaludo7 = (TextView) findViewById(R.id.lblsaludo7);
+        TextView lblsaludo8 = (TextView) findViewById(R.id.lblsaludo8);
+        //Recuperamos la información pasada en el intent
+        Bundle bundle = this.getIntent().getExtras();
+
+//Construimos el mensaje a mostrar
+        lblsaludo.setText(" Cedula \n " + bundle.getString("ID"));
+        lblsaludo2.setText(" Nombres \n " + bundle.getString("NOMBRES"));
+        lblsaludo3.setText(" Fecha \n " + bundle.getString("FN"));
+        lblsaludo4.setText(" Ciudad \n " + bundle.getString("CIUDAD"));
+        lblsaludo5.setText(" Masculino \n " + bundle.getString("MASCULINO"));
+        lblsaludo6.setText(" Femenino \n " + bundle.getString("FEMENINO"));
+        lblsaludo7.setText(" Correo \n " + bundle.getString("CORREO"));
+        lblsaludo8.setText(" Telefono \n " + bundle.getString("TELEFONO"));
+    }
+
+}
--- app/src/main/java/com/example/tarea1controlescomunes/MainActivity.java
+++ app/src/main/java/com/example/tarea1controlescomunes/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.tarea1controlescomunes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void btnenviar(View view){
+        //Creamos el Intent
+        Intent intent = new Intent(MainActivity.this, DatosRecibidos.class);
+        EditText txtid = (EditText)findViewById(R.id.txtid);
+        EditText txtnombres = (EditText)findViewById(R.id.txtnombres);
+        EditText txtfecha = (EditText)findViewById(R.id.txtfecha);
+        EditText txtciudad = (EditText)findViewById(R.id.txtciudad);
+        RadioButton rdbmasculino = (RadioButton) findViewById(R.id.rdbmasculino);
+        RadioButton rdbfemenino = (RadioButton)findViewById(R.id.rdbfemenino);
+        EditText txtcorreo = (EditText)findViewById(R.id.txtcorreo);
+        EditText txttelefono = (EditText)findViewById(R.id.txttelefono);
+
+
+        //Creamos la información a pasar entre actividades - Pares Key-Value
+        Bundle b = new Bundle();
+
+        b.putString("ID", txtid.getText().toString());
+        b.putString("NOMBRES", txtnombres.getText().toString());
+        b.putString("FN", txtfecha.getText().toString());
+        b.putString("CIUDAD", txtciudad.getText().toString());
+        b.putString("MASCULINO", rdbmasculino.getText().toString());
+        b.putString("FEMENINO", rdbfemenino.getText().toString());
+        b.putString("CORREO", txtcorreo.getText().toString());
+        b.putString("TELEFONO", txttelefono.getText().toString());
+
+        //Añadimos la información al intent
+        intent.putExtras(b);
+        // Iniciamos la nueva actividad
+        startActivity(intent);
+
+    }
+
 }
